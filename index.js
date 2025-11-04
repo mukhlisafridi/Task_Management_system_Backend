e7f7e6243f90c9db5f1e19704ed9f48f286b16d4
mukhlisafridi <mukhlisafridi4@gmail.com>
2025-11-04 15:43:31 +0500

    backend setup

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ import express from "express"
 import dotenv from "dotenv"
 import morgan from "morgan"
 import {connectDB} from "./utils/db.js"
+import cors from "cors"
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json())
+app.use(cors({
+  origin:process.env.FRONTEND_URL,
+  methods:["GET","POST","PUT","DELETE"],
+  allowedHeaders:["Content-Type","Authorization"]
+}))
 app.use(morgan("dev"))
 connectDB()
 app.get('/', (req, res) => {
