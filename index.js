cec3cdeed40c66df8e27a9dba34a912f76d981e3
mukhlisafridi <mukhlisafridi4@gmail.com>
2025-12-03 16:54:48 +0500

    completed

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://task-management-system-frontend-rho.vercel.app"],
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
