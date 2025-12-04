0f21e4170ab0230b4ec4e53d17c1f34d70f44158
mukhlisafridi <mukhlisafridi4@gmail.com>
2025-12-04 11:27:42 +0500

    updated

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://task-management-system-frontend-afridi.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
